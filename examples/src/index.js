2697bf8a9897fb758822c54b026689a57ccd19d1
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-03-12 11:41:53 +0100

    install eslint

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+
 import React from 'react';
-import { render} from 'react-dom';
-import {TheComp} from '../../src';
+import { render } from 'react-dom';
+import { TheComp } from '../../src';
 
 const App = () => (
-    <TheComp />
+  <TheComp />
 );
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './styles.css';
 
 export const TheComp = () => (
+  <div>
     <h1>Hello from My Component</h1>
+  </div>
 );
