792913d1ab082cada5add41b6f93f4164cdec57b
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-03-23 16:26:39 +0100

    shuffle features and add holding confirmations

--- src/pricing-text.js
+++ src/pricing-text.js
@@ -2,9 +2,10 @@ export const getCompanyFeaturePricing = () => [
   { text: 'Swiss privacy & security' },
   { text: 'Cap table' },
   { text: 'Basic reporting' },
-  { text: 'Basic modeling' },
   { text: 'Branded reporting', startup: false, scaleup: false },
+  { text: 'Basic modeling' },
   { text: 'Advanced modeling', startup: false },
+  { text: 'Holding confirmations', startup: false },
   { text: 'Equity plans', startup: false },
   { text: 'Employee dashboard', startup: false, scaleup: false },
   {
@@ -14,6 +15,14 @@ export const getCompanyFeaturePricing = () => [
     enterprise: 'Unlimited',
   },
   { text: 'Data room sharing', startup: false },
+  { text: 'Standard e-signatures', startup: false },
+  { text: 'Qualified e-signatures', startup: false, scaleup: false },
+  {
+    text: 'Document templating',
+    startup: false,
+    scaleup: false,
+    enterprise: 'Coming soon',
+  },
   {
     text: 'Admin seats',
     startup: '2',
@@ -35,12 +44,4 @@ export const getCompanyFeaturePricing = () => [
   { text: 'Onboarding data audit', startup: false, scaleup: false },
   { text: 'Dedicated account manager', startup: false, scaleup: false },
   { text: 'SLA', startup: false, scaleup: false },
-  { text: 'Standard e-signatures', startup: false },
-  { text: 'Qualified e-signatures', startup: false, scaleup: false },
-  {
-    text: 'Document templating',
-    startup: false,
-    scaleup: false,
-    enterprise: 'Coming soon',
-  },
 ];
