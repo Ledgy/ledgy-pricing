9a1c0f5a866d1c49ef93a6e8012b0b6d0549edcd
Marius Colacioiu <marius.colacioiu@gmail.com>
2020-04-08 11:16:03 +0200

    Allow to pass optional & customizable scaleup text

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { LedgyPricing, FEATURES } from '../../src';
 
 const App = () => (
-  <LedgyPricing highlightFeature={FEATURES.SUPPORT}/>
+  <LedgyPricing highlightFeature={FEATURES.SUPPORT} scaleupText="14 days free trial" />
 );
 
 render(<App />, document.getElementById('root'));
